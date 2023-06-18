9a5b471aab40ef0ef5d80a566d7bf40a3a93feef
msa <msai.sadehi@gmail.com>
2023-06-18 15:59:26 +0330

    sadeghi

--- snake_game.py
+++ snake_game.py
@@ -15,11 +15,16 @@ def move():
     if snake_head.direction == "up":
         ypos = snake_head.ycor()
         snake_head.sety(ypos + 20)
+    if snake_head.direction == "down":
+        ypos = snake_head.ycor()
+        snake_head.sety(ypos - 20)
 
-    # TODO  نوشتن سه شرط برای سمت چپ و پائین و بالا
     if snake_head.direction == "right":
         xpos = snake_head.xcor()
         snake_head.setx(xpos + 20)
+    if snake_head.direction == "left":
+        xpos = snake_head.xcor()
+        snake_head.setx(xpos - 20)
 
 
 main_surface = Screen()
@@ -31,6 +36,21 @@ snake_head = make_turtle("square", "blue")
 snake_head.goto(0, 100)
 snake_head.direction = "stop"
 food = make_turtle("circle", "red")
+
+def go_up():
+    snake_head.direction = "up"
+def go_left():
+    snake_head.direction = "left"
+def go_right():
+    snake_head.direction = "right"
+def go_down():
+    snake_head.direction = "down"
+
+main_surface.listen()
+main_surface.onkeypress(go_up,"Up")
+main_surface.onkeypress(go_down,"Down")
+main_surface.onkeypress(go_right,"Right")
+main_surface.onkeypress(go_left,"Left")
 
 running = True
 while running == True:
