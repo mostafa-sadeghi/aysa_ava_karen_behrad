2e077e24954da0c39f572446a342c5e1e92c6f8a
msa <msai.sadehi@gmail.com>
2023-06-24 19:56:03 +0330

    snake

--- snake_game.py
+++ snake_game.py
@@ -1,5 +1,9 @@
 from turtle import Screen, Turtle
 from time import sleep
+from random import randint
+
+
+score = 0
 
 
 def make_turtle(turtle_shape, turtle_color):
@@ -27,33 +31,68 @@ def move():
         snake_head.setx(xpos - 20)
 
 
+def change_food_position():
+    xposition = randint(-280, 280)
+    yposition = randint(-280, 280)
+    food.goto(xposition, yposition)
+
+
 main_surface = Screen()
 main_surface.bgcolor('black')
 main_surface.setup(width=600, height=600)
 main_surface.title("Snake Game")
+main_surface.tracer(False)
 
 snake_head = make_turtle("square", "blue")
 snake_head.goto(0, 100)
 snake_head.direction = "stop"
 food = make_turtle("circle", "red")
 
+
+score_turtle = make_turtle("square", "white")
+score_turtle.hideturtle()
+score_turtle.goto(0, 260)
+
+
 def go_up():
     snake_head.direction = "up"
+
+
 def go_left():
     snake_head.direction = "left"
+
+
 def go_right():
     snake_head.direction = "right"
+
+
 def go_down():
     snake_head.direction = "down"
 
+
+def reset():
+    pass
+
+
 main_surface.listen()
-main_surface.onkeypress(go_up,"Up")
-main_surface.onkeypress(go_down,"Down")
-main_surface.onkeypress(go_right,"Right")
-main_surface.onkeypress(go_left,"Left")
+main_surface.onkeypress(go_up, "Up")
+main_surface.onkeypress(go_down, "Down")
+main_surface.onkeypress(go_right, "Right")
+main_surface.onkeypress(go_left, "Left")
 
 running = True
 while running == True:
+    score_turtle.clear()
+    score_turtle.write(f"Score: {score}", align="center", font=48)
+
     main_surface.update()
+    if snake_head.distance(food) < 20:
+        score += 1
+
+        change_food_position()
+
+    if snake_head.xcor() > 290 or snake_head.xcor() < -290 or snake_head.ycor() > 290 or snake_head.ycor() < -290:
+        reset()
+
     move()
     sleep(0.2)
