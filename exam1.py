d916e3d89998f49cb724e3361e62b3155c94b7e2
msa <msai.sadehi@gmail.com>
2023-07-04 16:06:29 +0330

    sadeghi

---
+++ exam1.py
@@ -0,0 +1,27 @@
+# name = input("enter your name: ")
+# family = input("enter your family: ")
+# total = 0
+# for i in range(3):
+#     new_score = float(input("enter your score: "))
+#     total += new_score
+
+# print(f"{name} {family}'s total score is: {total}")
+# print(f"{name} {family} average is: {total/3}")
+
+# name = input("enter a name: ")
+# for i in range(len(name) - 1, -1, -1):
+#     print(name[i], end="")
+
+# names = []
+# for i in range(5):
+#     new_name = input("enter a name: ")
+#     names.append(new_name)
+
+# print(names)
+# new_name = input("enter a name to add in the list: ")
+# names.append(new_name)
+# print(names)
+# new_name = input("enter a name to delete from the list: ")
+# names.remove(new_name)
+# print(names)
+
